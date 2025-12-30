eb85319f939ede24d3cf4c0fb8abd91c39cb83b0
Chris Harper <chris-harper@inbox.ru>
2025-12-30 16:31:08 +0300

    task1 and task2 completed

--- homework/tasks/task_2.py
+++ homework/tasks/task_2.py
@@ -1,8 +1,10 @@
+import asyncio
+
 async def magic_func() -> int:
     return 42
 
 
 async def fix_this_code() -> int:
     # С этой функцией что-то не так, необходимо разобраться что именно и починить её.
-    # FIX THIS CODE
-    return magic_func()
+    result = await asyncio.create_task(magic_func())
+    return result
