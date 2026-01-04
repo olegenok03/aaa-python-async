079009e8ed64b64fd90865f64e8e17dafab93191
Chris Harper <chris-harper@inbox.ru>
2026-01-04 06:32:13 +0300

    task3, task4, task6 completed; task5 and task7 sometimes fail tests

--- homework/tasks/task_7.py
+++ homework/tasks/task_7.py
@@ -1,5 +1,6 @@
 import abc
 import asyncio
+from concurrent.futures import ThreadPoolExecutor
 
 
 class AbstractModel:
@@ -9,8 +10,9 @@ class AbstractModel:
 
 
 class Handler:
-    def __init__(self, model: AbstractModel):
+    def __init__(self, model: AbstractModel, max_workers: int = 12):
         self._model = model
+        self._executor = ThreadPoolExecutor(max_workers=max_workers)
 
     async def handle_request(self) -> None:
         # Модель выполняет некий тяжёлый код (ознакомьтесь с ним в файле тестов),
@@ -18,5 +20,6 @@ class Handler:
         #
         # Тест проверяет, что время исполнения одной корутины handle_request не слишком сильно
         # отличается от времени исполнения нескольких таких корутин, запущенных конкурентно.
-        #
-        # YOU CODE GOES HERE
+
+        loop = asyncio.get_event_loop()
+        await loop.run_in_executor(self._executor, self._model.compute)
